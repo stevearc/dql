2c472b5d3a89d4ca95e9565a1037e874aa9c7a46
Steven Arcangeli <stevearc@stevearc.com>
2016-05-18 19:00:45 -0700

    Bump version: 0.5.17 → 0.5.18

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ project = u'dql'
 copyright = u'2013, Steven Arcangeli'
 github_user = u'mathcamp'
 
-release = '0.5.17'
+release = '0.5.18'
 version = '.'.join(release.split('.')[:2])
 
 exclude_patterns = ['_build']
--- dql/__init__.py
+++ dql/__init__.py
@@ -7,7 +7,7 @@ import logging.config
 from .cli import DQLClient
 from .engine import Engine, FragmentEngine
 
-__version__ = '0.5.17'
+__version__ = '0.5.18'
 
 
 LOG_CONFIG = {
