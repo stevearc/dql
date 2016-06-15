1d41f8c6895d10740d0dec00949de8d352357e0e
Steven Arcangeli <stevearc@stevearc.com>
2016-06-15 11:24:51 -0700

    Bump version: 0.5.18 → 0.5.19

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ project = u'dql'
 copyright = u'2013, Steven Arcangeli'
 github_user = u'mathcamp'
 
-release = '0.5.18'
+release = '0.5.19'
 version = '.'.join(release.split('.')[:2])
 
 exclude_patterns = ['_build']
