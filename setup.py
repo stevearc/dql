a00f6542d71abd5aeaf1992570701333b0029c34
Ilya Konstantinov <ilya.konstantinov@gmail.com>
2019-10-21 14:00:30 -0400

    Bump version: 0.5.26 → 0.5.27

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ EXTRAS = {
 if __name__ == "__main__":
     setup(
         name="dql",
-        version="0.5.26",
+        version="0.5.27",
         description="DynamoDB Query Language",
         long_description=README + "\n\n" + CHANGES,
         classifiers=[
