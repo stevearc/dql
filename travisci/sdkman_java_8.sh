46867998198575df2b928d2da24194a8f90447de
Sai Jonnala <jspreddy@users.noreply.github.com>
2020-09-06 18:58:12 -0400

    CI is finally working with the proper java version

--- travisci/sdkman_java_8.sh
+++ travisci/sdkman_java_8.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# nosetests dynamo plugin requires Java 8 or order to work properly.
+# However, TravisCI's xenial ships with Java 11 and Java can't be set
+# with `jdk` when python is selected as language.
+# Other ways of setting the jdk have not panned out.
+# So, had to use sdkman.
+
 echo "---------------------------------------"
 echo "Installing sdkman ....................."
 echo "---------------------------------------"
@@ -15,7 +22,8 @@ echo "---------------------------------------"
 echo "Installing java using sdkman..........."
 echo "---------------------------------------"
 sdk install java 8.0.265.hs-adpt < /dev/null
-sdk current javasdk
+sdk current java
 echo "---------------------------------------"
+echo "java -version"
 java -version
 echo "---------------------------------------"
